6368f81f44e95a5dd136c959376092fe1e915331
SenseiZarkoDukic <zarko.dukic@gmail.com>
2024-03-17 22:00:05 +0100

    Refactor code to use single quotes and update promise handling

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
-const fs = require("fs");
-const superagent = require("superagent");
+const fs = require('fs');
+const superagent = require('superagent');
 
-fs.readFile(`${__dirname}/dog.txt`, "utf8", (err, data) => {
+fs.readFile(`${__dirname}/dog.txt`, 'utf8', (err, data) => {
   console.log(`Breed: ${data}`);
 
   superagent
     .get(`https://dog.ceo/api/breed/${data}/images/random`)
-    .end((err, res) => {
+    .then((res) => {
       if (err) return console.log(err.message);
       console.log(res.body.message);
 
-      fs.writeFile("dog-img.txt", res.body.message, (err) => {
+      fs.writeFile('dog-img.txt', res.body.message, (err) => {
         if (err) return console.log(err.message);
-        console.log("Random dog image saved to file!");
+        console.log('Random dog image saved to file!');
       });
-    });
+    })
+    .end((err, res) => {});
 });
