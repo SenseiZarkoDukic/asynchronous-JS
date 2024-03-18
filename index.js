845d20ebe1198aeecd5b6dbecc3fd2b665e485f5
SenseiZarkoDukic <zarko.dukic@gmail.com>
2024-03-18 18:09:38 +0100

    Remove error check in API call

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ fs.readFile(`${__dirname}/dog.txt`, 'utf8', (err, data) => {
   superagent
     .get(`https://dog.ceo/api/breed/${data}/images/random`)
     .then((res) => {
-      if (err) return console.log(err.message);
       console.log(res.body.message);
 
       fs.writeFile('dog-img.txt', res.body.message, (err) => {
