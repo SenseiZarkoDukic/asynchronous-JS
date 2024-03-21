93b0f57525671da1e7fb99401253ebd60a188eae
SenseiZarkoDukic <zarko.dukic@gmail.com>
2024-03-21 17:11:30 +0100

    Refactor code to use promises for file reading and writing

--- index.js
+++ index.js
@@ -10,20 +10,29 @@ const readFilePro = (file) => {
   });
 };
 
-readFilePro(`${__dirname}/dog.txt`).then((data) => {
-  console.log(`Breed: ${data}`);
-
-  superagent
-    .get(`https://dog.ceo/api/breed/${data}/images/random`)
-    .then((res) => {
-      console.log(res.body.message);
-
-      fs.writeFile('dog-img.txt', res.body.message, (err) => {
-        if (err) return console.log(err.message);
-        console.log('Random dog image saved to file!');
-      });
-    })
-    .catch((err) => {
-      console.log(err.message);
+const writeFilePro = (file, data) => {
+  return new Promise((resolve, reject) => {
+    fs.writeFile(file, data, (err) => {
+      if (err) reject('Could not write file 😢');
+      resolve('success');
     });
-});
+  });
+};
+
+readFilePro(`${__dirname}/dog.txt`)
+  .then((data) => {
+    console.log(`Breed: ${data}`);
+
+    return superagent.get(`https://dog.ceo/api/breed/${data}/images/random`);
+  })
+  .then((res) => {
+    console.log(res.body.message);
+
+    fs.writeFile('dog-img.txt', res.body.message, (err) => {
+      if (err) return console.log(err.message);
+      console.log('Random dog image saved to file!');
+    });
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
